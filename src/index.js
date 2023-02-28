77ea8c539f0b1b6bc8b521360366a0a4a4544489
ekkaira <denysxchebotar@gmail.com>
2023-02-28 14:40:32 +0200

    refactored components and files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
