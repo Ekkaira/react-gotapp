6ede4519bf0fca6febf37533cd54c00a2b75a895
ekkaira <denysxchebotar@gmail.com>
2023-03-01 14:44:11 +0200

    Added spinner and implementing error component

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-spin-wux3gq6scpc">
+      <div class="ldio-gr56o1ky75e">
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
