00224f9aae3928d86c2368c4495af9fced36a6aa
ekkaira <denysxchebotar@gmail.com>
2023-03-11 16:04:43 +0200

    finished routing and pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,7 +3,7 @@ import { Col, Row, Container } from "reactstrap";
 import Header from "../header";
 import RandomChar from "../randomChar";
 import ErrorMessage from "../errorMessage";
-import { CharacterPage, BooksPage, HousesPage, BooksItem } from "../pages";
+import { CharacterPage, BooksPage, HousesPage } from "../pages";
 import gotService from "../services/gotService";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -57,12 +57,6 @@ export default class App extends Component {
               <Route path="/characters" element={<CharacterPage />} />
               <Route path="/houses" element={<HousesPage />} />
               <Route path="/books" exact element={<BooksPage />} />
-              <Route
-                path="/books/:id"
-                render={({ match, location, history }) => {
-                  return <BooksItem />;
-                }}
-              />
             </Routes>
           </Container>
         </div>
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,5 @@
 import BooksPage from "./booksPage";
 import CharacterPage from "./characterPage";
 import HousesPage from "./housesPage";
-import BooksItem from "./booksItem";
-export { BooksPage, CharacterPage, HousesPage, BooksItem };
+
+export { BooksPage, CharacterPage, HousesPage };
